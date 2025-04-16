f8e84f134024920e2bb7edac7220967769eed603
IceDixx <kasai5208@gmail.com>
2025-04-16 14:49:47 +0800

    first time

--- src/main/java/raisetech/Student/management/Application.java
+++ src/main/java/raisetech/Student/management/Application.java
@@ -1,21 +1,31 @@
 package raisetech.Student.management;
 
 import ch.qos.logback.core.util.StringUtil;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-	public static void main(String[] args) {
 
+	private Map<String,Integer> student = new HashMap<>();
+
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello World";
+	@GetMapping("/Student")
+	public Map<String,Integer> student() {
+		return student;
+	}
+	@PostMapping("/Student")
+	public String addStudentInfo(String name , 	Integer age){
+		student.put(name,age);
+		return "got it~";
 	}
 }
