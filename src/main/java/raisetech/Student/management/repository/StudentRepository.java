66777c34f23c127412efbc8fe68ff0ea02481569
IceDixx <kasai5208@gmail.com>
2025-05-21 15:50:13 +0800

    第11回　Service&Controller

--- src/main/java/raisetech/Student/management/repository/StudentRepository.java
+++ src/main/java/raisetech/Student/management/repository/StudentRepository.java
@@ -1,12 +1,15 @@
-package raisetech.Student.management;
+package raisetech.Student.management.repository;
 
 import java.util.Date;
 import java.util.List;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+import raisetech.Student.management.data.Student;
+import raisetech.Student.management.data.studentCourses;
 
 @Mapper
 public interface StudentRepository {
@@ -14,25 +17,34 @@ public interface StudentRepository {
   @Select("SELECT * FROM student WHERE name = #{name}")
   Student searchByName(String name);
 
+  @Select("SELECT * FROM student WHERE age > #{age}")
+  List<Student> getYearsStudent(@Param("age") int age);
+
+  @Select("SELECT * FROM student_courses WHERE course_name = #{CourseName}")
+  List<studentCourses> getSelectCourse(@Param("CourseName") String CourseName);
+
   @Select("SELECT * FROM student")
   List<Student> getAllStudents();
+
+  @Select("SELECT * FROM student_courses")
+  List<studentCourses> getstudentCourses();
 
   @Select("SELECT * FROM student WHERE id = #{id}")
   Student searchById(int id);
 
   @Select("SELECT * FROM student_courses WHERE course_id = #{course_id}")
-  Student searchCourse(String course_name);
+  Student searchCourse(String courseName);
 
   @Select("SELECT * FROM student_courses WHERE course_id = #{course_id}")
-  Student searchCourseID(int course_id);
+  Student searchCourseID(int courseId);
 
-  @Insert("INSERT INTO student values(#{id}#{name},#{age},#{email},#{address},#{nickname},#{gender}")
-  void registerStudent(int id, String name, int age, String email, String address, String nickname,
+  @Insert("INSERT INTO student (name, age, email, address, nickname, gender) VALUES (#{name}, #{age}, #{email}, #{address}, #{nickname}, #{gender})")
+  void registerStudent(String name, int age, String email, String address, String nickname,
       String gender);
 
   @Insert("INSERT INTO student_courses (student_id, course_name, start_date, end_date) " +
-      "VALUES (#{student_id}, #{course_name}, #{start_date}, #{end_date})")
-  void registerStudentCourses(int student_id, String course_name, Date start_date, Date end_date);
+      "VALUES (#{studentId}, #{courseName}, #{startDate}, #{endDate})")
+  void registerStudentCourses(Integer studentId, String courseName, Date startDate, Date endDate);
 
 
   @Update("UPDATE student_courses SET student_id=#{id} WHERE id = #{id}")
@@ -43,5 +55,5 @@ public interface StudentRepository {
 
 
   @Delete("DELETE FROM student_courses WHERE course_name = #{course_name}")
-  void deleteCourse(String course_name);
+  void deleteCourse(String courseName);
 }
