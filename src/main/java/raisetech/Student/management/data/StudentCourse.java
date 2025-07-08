ea9d90386ff5cd739330e3aad8b6a6f1a3fb8684
IceDixx <kasai5208@gmail.com>
2025-07-08 10:57:14 +0800

    第19回　課題提出

--- src/main/java/raisetech/Student/management/data/StudentCourse.java
+++ src/main/java/raisetech/Student/management/data/StudentCourse.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class StudentsCourses {
+public class StudentCourse {
 
   private int courseId;
   private int studentId;
--- src/main/java/raisetech/Student/management/domain/StudentCoursesDetail.java
+++ src/main/java/raisetech/Student/management/domain/StudentCoursesDetail.java
@@ -2,11 +2,11 @@ package raisetech.Student.management.domain;
 
 import java.util.List;
 import raisetech.Student.management.data.Student;
-import raisetech.Student.management.data.StudentsCourses;
+import raisetech.Student.management.data.StudentCourse;
 
 public class StudentCoursesDetail {
 
   private Student student;
-  private List<StudentsCourses> studentsCourses;
+  private List<StudentCourse> studentsCourses;
 
 }
